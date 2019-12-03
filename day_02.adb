b8afce333fdf6e993ec64f63a9922d87180f89ec
Jonny Weese <jweese@gmail.com>
2019-12-02 20:07:07 -0500

    day 02, part 2

--- day_02.adb
+++ day_02.adb
@@ -4,13 +4,24 @@ with Memory;
 
 procedure Day_02 is
    Mem: constant Memory.Block := Memory.Get;
-   M: Intcode.Machine := Intcode.New_Machine(Mem);
+
+   use type Memory.Value;
+   subtype Input is Memory.Value range 0..99;
 begin
-
-   -- Restore 1202 Alarm
-   M.Poke(Addr => 16#1#, Value => 12);
-   M.Poke(Addr => 16#2#, Value => 02);
-
-   M.Run;
-   Ada.Integer_Text_IO.Put(Integer(M.Peek(0)));
+   for Noun in Input'Range loop
+      for Verb in Input'Range loop
+         declare
+            M: Intcode.Machine := Intcode.New_Machine(Mem);
+         begin
+            M.Poke(Addr => 16#1#, Value => Noun);
+            M.Poke(Addr => 16#2#, Value => Verb);
+            M.Run;
+            if M.Peek(0) = 19690720 then
+               Ada.Integer_Text_IO.Put(Integer(Noun));
+               Ada.Integer_Text_IO.Put(Integer(Verb));
+               return;
+            end if;
+         end;
+      end loop;
+   end loop;
 end Day_02;
