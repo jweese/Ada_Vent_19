f5be97648c9b8696833cf794b475dc586e66bda8
Jonny Weese <jweese@gmail.com>
2019-12-05 01:54:17 -0500

    allow reading Memory.Block from file
    Default is stdin.

--- lib/memory.adb
+++ lib/memory.adb
@@ -1,10 +1,10 @@
 with Ada.Containers.Vectors;
-with Ada.Text_IO;
 with Ada.Integer_Text_IO;
 with Ada.IO_Exceptions;
 
 package body Memory is
-   function Read_Comma_Separated return Block is
+   function Read_Comma_Separated(
+         From: File_Type := Standard_Input) return Block is
       package Vec is new Ada.Containers.Vectors(
          Index_type => Address, Element_Type => Value);
       V: Vec.Vector := Vec.Empty_Vector;
@@ -12,10 +12,10 @@ package body Memory is
       Comma: Character;
    begin
       loop
-         Ada.Integer_Text_IO.Get(Curr);
+         Ada.Integer_Text_IO.Get(From, Curr);
          V.Append(Value(Curr));
-         exit when Ada.Text_IO.End_Of_File;
-         Ada.Text_IO.Get(Comma);
+         exit when Ada.Text_IO.End_Of_File(From);
+         Ada.Text_IO.Get(From, Comma);
          if Comma /= ',' then
             raise Ada.IO_Exceptions.Data_Error;
          end if;
--- lib/memory.ads
+++ lib/memory.ads
@@ -1,7 +1,11 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
 package Memory is
    type Address is new Natural;
    type Value is new Integer;
    type Block is array(Address range <>) of Value;
 
-   function Read_Comma_Separated return Block;
+   function Read_Comma_Separated(
+         From: File_Type := Standard_Input) return Block;
 end Memory;
