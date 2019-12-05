d9216513c112e077771dd0ea70c10e6afa19f155
Jonny Weese <jweese@gmail.com>
2019-12-04 22:24:27 -0500

    remove edge cases from double testing
    We can do this by padding the Image with blanks on either side of the
    six digit number.

--- day_04.adb
+++ day_04.adb
@@ -4,31 +4,19 @@ procedure Day_04 is
    type Cand is new Integer range 284639 .. 748759;
 
    function Is_Valid(C: Cand) return Boolean is
-      Image: String(1..6);
-      subtype Image_Edge is String(1..3);
+      Image: String(1..8) := (others => ' ');
       subtype Image_Middle is String(1..4);
 
-      function Has_Double_Begin(S: Image_Edge) return Boolean is
-         (S(1) = S(2) and S(2) /= S(3));
-
-      function Has_Double_End(S: Image_Edge) return Boolean is
-         (S(1) /= S(2) and S(2) = S(3));
-
-      function Has_Double_Middle(S: Image_Middle) return Boolean is
+      function Has_Double(S: Image_Middle) return Boolean is
          (S(1) /= S(2) and S(2) = S(3) and S(3) /= S(4));
 
       Non_Descending: Boolean := True;
-      Has_Double: Boolean := False;
+      Any_Has_Double: Boolean := False;
    begin
-      Ada.Integer_Text_IO.Put(To => Image, Item => Integer(C));
-
-      Has_Double :=
-         Has_Double_Begin(Image(1..3))
-         or Has_Double_End(Image(4..6))
-         or (for some I in 1 .. 3 => Has_Double_Middle(Image(I .. I + 3)));
-      Non_Descending := (for all I in 1 .. 5 => Image(I) <= Image(I + 1));
-
-      return Non_Descending and Has_Double;
+      Ada.Integer_Text_IO.Put(To => Image(2..7), Item => Integer(C));
+      Any_Has_Double := (for some I in 1 .. 5 => Has_Double(Image(I .. I + 3)));
+      Non_Descending := (for all I in 2 .. 6 => Image(I) <= Image(I + 1));
+      return Non_Descending and Any_Has_Double;
    end Is_Valid;
 
    Count: Natural := 0;
