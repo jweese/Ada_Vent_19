d1314c3e099f18b78a6cea9b4709d6def50ea8e7
Jonny Weese <jweese@gmail.com>
2019-12-08 22:35:58 -0500

    import intcode to ic2 (part 1)

--- lib/ic2.adb
+++ lib/ic2.adb
@@ -1,4 +1,9 @@
+with Intcode.Op;
+use Intcode.Op;
+
 package body IC2 is
+   use type Memory.Address;
+
    protected body Port is
       entry Put(I: in Integer) when Status /= Full is
       begin
@@ -26,7 +31,36 @@ package body IC2 is
 
    task body Executor is
       PC: Memory.Address := 0;
+
+      function Read(
+            From: Memory.Address;
+            Mode: Parameter_Mode) return Memory.Value is
+         V: constant Memory.Value := AM.Mem(From);
+
+      begin
+         return (case Mode is
+            when Immediate => V,
+            when Position => AM.Mem(Memory.Address(V)));
+      end Read;
    begin
+      loop
+         declare
+            Curr_Op: constant Schema := Decode(AM.Mem(PC));
+            Params: array (Curr_Op.Params'Range) of Memory.Value;
+            Store_To: constant Memory.Address := Memory.Address(
+                  Read(
+                     From => PC + Memory.Address(Params'Last),
+                     Mode => Immediate));
+         begin
+            for I in Params'Range loop
+               Params(I) := Read(
+                  From => PC + Memory.Address(I), Mode => Curr_Op.Params(I));
+            end loop;
+
+            exit;
+         end;
+      end loop;
+
       AM.Input.Close;
       AM.Output.Close;
    end Executor;
